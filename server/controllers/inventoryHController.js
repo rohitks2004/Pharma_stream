815ea345214b53c53557460728db1ca80ec28637
Sakthi-007 <sakthisiva2004@gmail.com>
2024-09-03 00:05:02 +0530

    commit

--- server/controllers/inventoryHController.js
+++ server/controllers/inventoryHController.js
@@ -12,7 +12,7 @@ exports.addInventory = async (req, res) => {
 
 exports.getInventories = async (req, res) => {
     try {
-        const inventories = await Inventory.find().populate('dealerId');
+        const inventories = await Inventory.find().populate();
         res.status(200).json(inventories);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -40,7 +40,8 @@ exports.updateInventory = async (req, res) => {
 
 exports.deleteInventory = async (req, res) => {
     try {
-        await Inventory.findByIdAndDelete(req.params.id);
+       await Inventory.deleteOne({medicineId:req.params.id});
+
         res.status(204).json({ message: "Inventory deleted" });
     } catch (err) {
         res.status(500).json({ error: err.message });
--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use('/api/cart',cartRoutes);
 app.use('/app/billing',billingRoutes);
 app.use('/api/cat',catRoutes);
 app.use('/api/dinventory',inventoryDRoutes);
-app.use('api/hinventory',inventoryHRoutes)
+app.use('/api/hinventory',inventoryHRoutes)
 
 
 
